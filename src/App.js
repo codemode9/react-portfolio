2b58028ede9846ef820c8e4ae6edcaa2d942e76a
codemode9 <timourbasegmez@hotmail.com>
2018-04-16 21:26:24 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from "./components/Navigation";
+
+const App = () => (
+<Navigation/>
+);
+
+export default App;

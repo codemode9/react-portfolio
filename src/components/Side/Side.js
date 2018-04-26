253875fd8d3ecc156e5451b0d8dee93d4530278f
codemode9 <timourbasegmez@hotmail.com>
2018-04-26 15:27:03 -0400

    sidebox links

--- src/components/Side/Side.js
+++ src/components/Side/Side.js
@@ -9,8 +9,12 @@ const Side = (props) => {
       
       
         <Card body className="sidebox">
-        <img src={require('../../images/linkedin.png')} className='linky' style={{width: 65, height: 65}}></img>
-        <img src={require('../../images/octocat.png')} className='kitty' style={{width: 150, height: 100}}></img>
+            <a href={'https://www.linkedin.com/in/timour-basegmez/'} target={'_blank'}>
+                <img src={require('../../images/linkedin.png')} className='linky' style={{width: 65, height: 65}} />
+            </a>
+            <a href={'https://github.com/codemode9'} target={'_blank'}>
+                <img src={require('../../images/octocat.png')} className='kitty' style={{width: 150, height: 100}} href={'https://www.google.com'}/>
+            </a>
         </Card>
     
     
@@ -18,3 +22,5 @@ const Side = (props) => {
 };
 
 export default Side;
+
+
